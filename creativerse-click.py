d2214919fcfed958265c0d0fad9864f91a502797
dixx <jenzdixx@googlemail.com>
2018-02-24 14:29:41 +0100

    Fix minor bugs

--- creativerse-click.py
+++ creativerse-click.py
@@ -4,8 +4,8 @@ import pyautogui, time
 print('Press Ctrl-C to quit.')
 time.sleep(27)
 for i in range(3):
-    print '\a'
+    print('\a')
     time.sleep(1)
 
 x, y = pyautogui.position()
-mouseDown(x, y, 'left')
+pyautogui.mouseDown(x, y, 'left')
