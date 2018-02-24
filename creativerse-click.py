4c20bed221b6554e7097eeca40d088d111e642ec
dixx <jenzdixx@googlemail.com>
2018-02-24 14:09:41 +0100

    Add startup waiting time with beeps

--- creativerse-click.py
+++ creativerse-click.py
@@ -1,7 +1,11 @@
 #! python3
-import pyautogui
+import pyautogui, time
 
 print('Press Ctrl-C to quit.')
+time.sleep(27)
+for i in range(3):
+    print '\a'
+    time.sleep(1)
 
 # Get and print the mouse coordinates.
 x, y = pyautogui.position()
